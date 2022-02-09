4981c8a3ac710c927772e284eb4e7edb09556784
camposecobusiness <67613124+CamposecoKevin@users.noreply.github.com>
2022-02-09 17:06:22 -0600

    Create Portafolio_Eficiente.R

---
+++ Portafolio_Eficiente.R
@@ -0,0 +1,25 @@
+#Veámos como optimizar este portafolio (maximizando S) en un caso en que solo vamos a cambiar w3,w4
+rm(list = ls())
+
+library(portfolioAnalytics)
+
+precios <- NULL
+
+tickers <- c("AMZN", "AAPL", "NFLX", "FB")
+
+for (nom in tickers) {
+    precios <- cbind(precios,getSymbols(src="yahoo", from ="2016-01-02", to ="2019-06-01", periodicity = "daily", auto.assign=FALSE)[,c(4)])
+}
+
+options(scipen = 999)
+retornados <- na.omit(ROC(precios))
+ret<- c("AMZN", "AAPL", "NFLX", "FB")
+colnames(retornados)<- ret
+
+ 
+Sharper.indiv1<-SharpeRatio(R=retornados, Rf=0, FUN = "StDev") #Obtengamos el ratio de sharpe de cada activo
+
+#Optimizamos ahora el portafolio
+
+
+#Optimizando desde otro equipo en R
